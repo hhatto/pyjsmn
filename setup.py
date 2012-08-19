96ee23ef09165e8666976790679c190ac1f84889
hhatto <hhatto.jp@gmail.com>
2012-08-20 01:31:49 +0900

    update

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from distutils.core import setup, Extension
 
 
-import os
 setup(
     name='pyjsmn',
     version='0.1',
     description='Python extension for jsmn(JSON parser).',
+    license='Expat License',
     author='Hideo Hattori',
     author_email='hhatto.jp@gmail.com',
-    #url='',
+    url='https://github.com/hhatto/pyjsmn',
     ext_modules=[
         Extension('pyjsmn',
                   sources=['pyjsmn/_jsmn.c', ],
                   include_dirs=['./jsmn/'],
                   library_dirs=['./jsmn/'],
-                  libraries=['jsmn'],
-                 )]
+                  libraries=['jsmn'])]
 )
