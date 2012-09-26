ac36163ac373e62557449edad6841faa1244bc3a
Hideo Hattori <hhatto.jp@gmail.com>
2012-09-27 01:20:06 +0900

    add README

--- setup.py
+++ setup.py
@@ -5,15 +5,18 @@ setup(
     name='pyjsmn',
     version='0.1',
     description='Python extension for jsmn(JSON parser).',
+    long_description=open('README.rst').read(),
     license='Expat License',
     author='Hideo Hattori',
     author_email='hhatto.jp@gmail.com',
     url='https://github.com/hhatto/pyjsmn',
     ext_modules=[
         Extension('pyjsmn',
-                  sources=['pyjsmn/_jsmn.c'],
+                  sources=['pyjsmn/_jsmn.c', './jsmn/jsmn.c'],
                   include_dirs=['./jsmn/'],
-                  library_dirs=['./jsmn/'],
-                  extra_compile_args=["-DDEBUG"],
-                  libraries=['jsmn'])]
+                  #library_dirs=['./jsmn/'],
+                  #extra_compile_args=["-DDEBUG"],
+                  #extra_compile_args=["-DJSMN_STRICT"],
+                  #libraries=['jsmn'])]
+                  )]
 )
